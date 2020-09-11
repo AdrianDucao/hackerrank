b81375ad2558ae95a0b3888163ace698b7820d93
AdrianDucao <adrianducao@gmail.com>
2020-09-11 09:06:06 +0800

    playing-with-characters

---
+++ c/playing-with-characters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char ch;
+	char s[60];
+	char sen[150];
+
+	scanf("%c", &ch);
+	scanf("%s", s);
+	scanf("\n");
+	scanf("%[^\n]%*c", sen);
+
+	printf("%c \n", ch);
+	printf("%s \n", s);
+	printf("%s", sen);
+
+	return 0;
+}
